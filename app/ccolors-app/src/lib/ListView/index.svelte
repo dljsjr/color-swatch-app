95d7233ec869f29e533f60ba8131c46d2f3efd85
Doug Stephen <dstephen@oxefit.com>
2021-06-26 15:36:23 -0500

    Convert HSV -> HSL and visualize, found an API bug.
    Implemented the data model while I was sleepy
    and forgot that hue is an angle not a percent :(

--- app/ccolors-app/src/lib/ListView/index.svelte
+++ app/ccolors-app/src/lib/ListView/index.svelte
@@ -1,9 +1,13 @@
 <script lang="ts">
     export let colors;
+
+    export function getHSLSat(value) {
+        return value.sat*value.val/((value.hue=(2-value.sat)*value.val)<1?value.hue:2-value.hue)
+    }
 </script>
 
 <div class="h-full w-full">
     {#each colors as {name, value }, i}
-        <p>Color named {name} has hsv ({value.hue}, {value.sat}, {value.val})</p>
+        <p style="color: hsl(0, {100 * getHSLSat(value)}%, {100 * (value.hue/2)}%);">{name}</p>
     {/each}
 </div>
