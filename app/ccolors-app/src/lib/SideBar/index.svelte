99f7bf486e9afa478dc9d5cf256021775862605e
Doug Stephen <dstephen@oxefit.com>
2021-06-26 14:41:46 -0500

    Some tidying + stub out sidebar.

---
+++ app/ccolors-app/src/lib/SideBar/index.svelte
@@ -0,0 +1,11 @@
+<script></script>
+
+<style>
+    #sidebar {
+        background-color: #D6D8D8;
+    }
+</style>
+
+<div class="h-full w-80" id="sidebar">
+
+</div>
--- app/ccolors-app/src/routes/index.svelte
+++ app/ccolors-app/src/routes/index.svelte
@@ -1,5 +1,9 @@
 <script lang="ts">
     import Header from '$lib/Header/index.svelte'
+    import SideBar from '$lib/SideBar/index.svelte'
 </script>
 
-<Header></Header>
+<div class="h-screen w-screen flex flex-col">
+    <Header></Header>
+    <SideBar></SideBar>
+</div>
