172333c9e160d59fddc71679cde908e8f6d58253
Doug Stephen <dstephen@oxefit.com>
2021-06-26 11:13:46 -0500

    Refactor color generating script, add Random mode

--- scripts/color_generator/color_gen.py
+++ scripts/color_generator/color_gen.py
@@ -5,6 +5,7 @@ import colorsys
 import urllib.request
 import psycopg2
 import sys
+import random
 
 CSV_URL = "https://unpkg.com/color-name-list/dist/colornames.bestof.csv"
 
@@ -38,35 +39,44 @@ def close_database_connection(db_conn, cursor):
     db_conn.close()
 
 
+def populate_database(color_rows, db_conn, cursor, number):
+    for (idx, row) in enumerate(color_rows):
+        color_name, (hue, sat, val) = color_row_to_record(row)
+
+        try:
+            cursor.execute(
+                "INSERT INTO colors ( name, value ) VALUES ( %s, ((%s)::colorPart,(%s)::colorPart,(%s)::colorPart)::colorHSV )", (color_name, hue, sat, val))
+        except psycopg2.errors.UniqueViolation:
+            print(
+                F"Color {color_name} already exists in DB, skipping")
+            db_conn.commit()
+        except:
+            print(
+                F"Unexpected database error while generating colors: {sys.exc_info()[0]}; aborting color generation")
+            break
+
+        if idx >= (number - 1):
+            break
+
+
 class ColorGenerator(object):
     """Generate the color swatch list using https://github.com/meodai/color-names"""
 
     def default(self, number=100):
         db_conn, cursor = create_database_connection()
 
-        for (idx, row) in enumerate(get_color_dict()):
+        populate_database(get_color_dict(), db_conn, cursor, number)
 
-            color_name, (hue, sat, val) = color_row_to_record(row)
-
-            try:
-                cursor.execute(
-                    "INSERT INTO colors ( name, value ) VALUES ( %s, ((%s)::colorPart,(%s)::colorPart,(%s)::colorPart)::colorHSV )", (color_name, hue, sat, val))
-            except psycopg2.errors.UniqueViolation:
-                print(
-                    F"Color {color_name} already exists in DB, skipping")
-                db_conn.commit()
-            except:
-                print(
-                    F"Unexpected database error while generating colors: {sys.exc_info()[0]}; aborting color generation")
-                close_database_connection(db_conn, cursor)
-                break
-
-            if idx >= (number - 1):
-                close_database_connection(db_conn, cursor)
-                break
+        close_database_connection(db_conn, cursor)
 
     def random(self, number=100):
-        print(f"Generating {number} random colors")
+        db_conn, cursor = create_database_connection()
+        colors = list(get_color_dict())
+        random.shuffle(colors)
+
+        populate_database(colors, db_conn, cursor, number)
+
+        close_database_connection(db_conn, cursor)
 
 
 if __name__ == '__main__':
