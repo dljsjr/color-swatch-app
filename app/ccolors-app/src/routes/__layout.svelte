3f760c3d04094ae762083807ad764502fddd7662
Doug Stephen <dstephen@oxefit.com>
2021-06-26 14:49:31 -0500

    Refactor sidebar and header to layout file.

--- app/ccolors-app/src/routes/__layout.svelte
+++ app/ccolors-app/src/routes/__layout.svelte
@@ -1,2 +1,13 @@
-<script>import "../app.postcss";</script>
-<slot></slot>
+<script>
+    import "../app.postcss"
+    import Header from '$lib/Header/index.svelte'
+    import SideBar from '$lib/SideBar/index.svelte'
+</script>
+
+<div class="h-screen w-screen flex flex-col">
+    <Header></Header>
+    <div class="h-full w-full flex flex-row">
+        <SideBar></SideBar>
+        <slot></slot>
+    </div>
+</div>
--- app/ccolors-app/src/routes/index.svelte
+++ app/ccolors-app/src/routes/index.svelte
@@ -1,9 +1,5 @@
 <script lang="ts">
-    import Header from '$lib/Header/index.svelte'
-    import SideBar from '$lib/SideBar/index.svelte'
+    import ListView from '$lib/ListView/index.svelte'
 </script>
 
-<div class="h-screen w-screen flex flex-col">
-    <Header></Header>
-    <SideBar></SideBar>
-</div>
+<ListView></ListView>
