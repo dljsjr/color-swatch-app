b819931a413d5051e1d80f6756288f6f54d44314
Doug Stephen <dstephen@oxefit.com>
2021-06-26 15:22:47 -0500

    Stub out loading first 25 colors

--- app/ccolors-app/src/lib/ListView/index.svelte
+++ app/ccolors-app/src/lib/ListView/index.svelte
@@ -1,5 +1,9 @@
-<script></script>
+<script lang="ts">
+    export let colors;
+</script>
 
 <div class="h-full w-full">
-    
+    {#each colors as {name, value }, i}
+        <p>Color named {name} has hsv ({value.hue}, {value.sat}, {value.val})</p>
+    {/each}
 </div>
--- app/ccolors-app/src/routes/index.svelte
+++ app/ccolors-app/src/routes/index.svelte
@@ -1,5 +1,31 @@
 <script lang="ts">
     import ListView from '$lib/ListView/index.svelte'
+
+    export let json;
 </script>
 
-<ListView></ListView>
+<script lang="ts" context="module">
+	/**
+	 * @type {import('@sveltejs/kit').Load}
+	 */
+	export async function load({ page, fetch, session, context }) {
+        const api_url = 'http://localhost:8000/colors?limit=25&start_at=1'
+        const api_response = await fetch(api_url)
+
+        if (api_response.ok) {
+            return {
+                props: {
+                    json: await api_response.json()
+                }
+            };
+        }
+
+        return {
+            status: api_response.status,
+            error: new Error(`Error getting color list from API ${api_response.error}`)
+        };
+
+    }
+</script>
+
+<ListView colors={json}></ListView>
