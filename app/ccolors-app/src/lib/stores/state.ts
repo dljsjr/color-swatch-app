f0427ff8c176615fe2f55905a8867c4b3d114dce
Doug Stephen <dstephen@oxefit.com>
2021-06-27 15:10:15 -0500

    Reduce limit (for now)

--- app/ccolors-app/src/lib/stores/state.ts
+++ app/ccolors-app/src/lib/stores/state.ts
@@ -1,5 +1,5 @@
 import { writable, readable } from 'svelte/store';
 
 export const totalColors = writable(0);
-export const colorsPerPage = readable(25);
+export const colorsPerPage = readable(16);
 export const totalPages = writable(0);
