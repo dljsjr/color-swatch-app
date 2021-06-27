240474e9eb865b0cc6f316689bb4e23040d8288d
Doug Stephen <dstephen@oxefit.com>
2021-06-27 16:57:02 -0500

    Tweak header logo position a bit.

--- app/ccolors-app/src/lib/Header/index.svelte
+++ app/ccolors-app/src/lib/Header/index.svelte
@@ -4,7 +4,7 @@
 
 <div class="header" id="header">
 	<a href="/"
-		><img src="/logo-symbol.svg" alt="Helpful Humans Logo" class="block h-4/5 w-14 ml-6" /></a
+		><img src="/logo-symbol.svg" alt="Helpful Humans Logo" class="block h-4/5 w-14 ml-8" /></a
 	>
 	<input bind:value={searchString} placeholder="Search" class="h-2/5 w-72 p-4 mr-6 rounded-md" />
 </div>
