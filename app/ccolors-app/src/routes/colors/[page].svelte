0fdbb54a71b188793fa8ef645241ff75a7383974
Doug Stephen <dstephen@oxefit.com>
2021-06-27 14:12:18 -0500

    Add pagination route.

---
+++ app/ccolors-app/src/routes/colors/[page].svelte
@@ -0,0 +1,44 @@
+<script lang="ts" context="module">
+	import { totalColors } from '$lib/stores/state';
+	/**
+	 * @type {import('@sveltejs/kit').Load}
+	 */
+	export async function load({ page, fetch, session, context }) {
+		const metadata_fetch = await fetch('/api/count');
+
+		if (metadata_fetch.ok) {
+			const count = (await metadata_fetch.json())['json']['count'];
+			totalColors.set(count);
+		} else {
+			return {
+				status: metadata_fetch.status,
+				error: new Error(`Error getting color list from API ${metadata_fetch.error}`)
+			};
+		}
+
+		let requestedPage = parseInt(page.params.page);
+		const colors_fetch = await fetch(`/api/colors/${requestedPage}`);
+		let colors_json = (await colors_fetch.json())['json'];
+
+		if (colors_fetch.ok) {
+			return {
+				props: {
+					json: colors_json
+				}
+			};
+		} else {
+			return {
+				status: colors_fetch.status,
+				error: new Error(`Error getting color list from API ${colors_fetch.error}`)
+			};
+		}
+	}
+</script>
+
+<script lang="ts">
+	import ListView from '$lib/ListView/index.svelte';
+
+	export let json;
+</script>
+
+<ListView colors={json} />
